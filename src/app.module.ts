3abfd853f4cd145b10e6e165600b8aec5827ab19
Jheison Novak <jheisonn22@hotmail.com>
2026-04-16 23:57:00 -0300

    feat(app): add global configuration module and update port environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { TaskModule } from "./modules/task/task.module";
 import { typeOrmConfig } from "./shared/database/database.config.service";
 
 @Module({
-	imports: [TypeOrmModule.forRoot(typeOrmConfig), TaskModule],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRoot(typeOrmConfig),
+		TaskModule,
+	],
 	controllers: [],
 	providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap(): Promise<void> {
 	const documentFactory = (): OpenAPIObject => SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup("api/docs", app, documentFactory);
 
-	await app.listen(process.env.APP_PORT ?? 3000);
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
